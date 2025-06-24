c349419bccc0c25ed9f5a48e1c79ea7889a66511
meerabsaif <meerabsaif@gmail.com>
2025-06-24 11:34:11 +0500

    The first training of python

---
+++ Day1Script.py
@@ -0,0 +1,33 @@
+# Internship Summer 2025 
+# Week 1 
+# Day 1 | June 23rd 2025 
+
+"""
+This File includes python programming fundamentals. 
+It is advised to use biological examples instead of simple ones to improve understanding.
+The main purpose is practice and documentation. 
+Here, I have used docstring to comment multiple lines. 
+"""
+
+#----------------- Variables : Containers storing data 
+# We use = operator to assign values. Values can be of any data type (string num int float bool)
+x= 5
+y= 8
+
+dna= "ATGCCAAT"
+rna= "AUGCCUAG"
+
+#----------------- Input Operations : Getting Data from the User 
+seq1dna= input("Enter DNA sequence: ")
+seq2rna= input("Enter RNA sequence: ")
+
+#----------------- Output Operations : Diplaying Data 
+print("sequence is:", seq1dna )
+print("sequence is:", seq2rna )
+
+#----------------- Operators : +,-,x,/ 
+
+
+
+
+
